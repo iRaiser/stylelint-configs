a7e1e97bd793de7ef228d2c2350d8b103a2aede9
Boris Damevin <bdamevin@iraiser.eu>
2021-01-12 14:36:06 +0100

    feat(scss): add comments rules

--- packages/stylelint-config-scss/index.js
+++ packages/stylelint-config-scss/index.js
@@ -6,6 +6,17 @@ module.exports = {
     ],
     'rules': {
         'scss/at-import-no-partial-leading-underscore': true,
+        'scss/comment-no-empty': true,
+        'scss/comment-no-loud': true,
+        'scss/double-slash-comment-empty-line-before': [
+            'always', {
+                ignore: [
+                    'between-comments',
+                    'stylelint-commands'
+                ]
+            }
+        ],
+        'scss/double-slash-comment-whitespace-inside': 'always',
         'scss/selector-no-redundant-nesting-selector': true
     }
 };
